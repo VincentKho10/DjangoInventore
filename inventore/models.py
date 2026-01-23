e2a1d6b7972b60022511c15649934aa0afb14f0f
pembe <pembe@ADMIN8PEMBELIAN>
2026-01-23 09:42:43 +0700

    adding models item_mutation_history

--- inventore/models.py
+++ inventore/models.py
@@ -7,7 +7,7 @@ from django_mongodb_backend.fields import EmbeddedModelField
 
 # Create your models here.
 
-class Unit(models.Model):
+class UnitMetric(models.Model):
     unit_name = models.CharField(max_length=30)
     
     def __str__(self):
@@ -29,7 +29,8 @@ class Tax(models.Model):
 
 class Item(models.Model):
     item_name = models.CharField(max_length=200)
-    unit = models.ForeignKey(Unit,on_delete=models.CASCADE)
+    item_code = models.CharField(max_length=200)
+    unit_metric = models.ForeignKey(UnitMetric,on_delete=models.DO_NOTHING)
     quantity = models.IntegerField(default=0)
     unit_price = models.DecimalField(default=0, decimal_places=2, max_digits=20) 
     base_total = models.DecimalField(editable=False, decimal_places=2, max_digits=20)
@@ -43,3 +44,20 @@ class Item(models.Model):
 
     def __str__(self):
         return self.item_name
+    
+class ItemMutationHistory(models.Model):
+    title = models.CharField(max_length=50, editable=False)
+    description = models.TextField(max_length=200, editable=False)
+    item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
+    quantity = models.DecimalField(decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        quantityresult = self.item.quantity + self.quantity
+        self.title = ("Increase " if self.quantity >= 0 else "Decrease ") + "%d at %s_%s by root ".format(self.quantity, self.item.item_code, self.item.item_name)
+        self.description = self.title + " from %d to %d".format(self.item.quantity, quantityresult) 
+        self.item.quantity = quantityresult
+        self.item.save()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
